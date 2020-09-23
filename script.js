dfeb608c9309aa6d5d5aa2c7f3059233c641c211
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-23 13:46:19 -0700

    finished project and ready for submit

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    let principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        // Decided to not add the feature below in case I get docked points
+        //document.getElementById("principal").value = "";
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } 
+    else {
+        let rate = document.getElementById("rate").value;
+        let years = document.getElementById("years").value;
+        let interest = principal * years * rate / 10;
+        let year_date =  new Date().getFullYear() + parseInt(years,10);
+        document.getElementById("result").innerHTML = 
+            "If you deposit <mark>" + principal + "</mark>,<br>\
+            at an interest rate of <mark>" + rate + "%</mark>.<br>\
+            You will receive an amount of <mark>" + interest + "</mark>,<br>\
+            in the year <mark>" + year_date + "</mark>";
+    }
 }
-        
+     
+function readSlider() {
+    document.getElementById("rateDisplay").innerHTML = document.getElementById("rate").value + "%";
+}
